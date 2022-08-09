ca6872eac6f18d21b16abe789ac301e4657af285
Зульфина <lycorisfatalis@gmail.com>
2022-08-09 11:12:01 +0300

    Исправлена опечатка

--- Library/Program.cs
+++ Library/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IReader<Book>>(reader => new Library.Services.JsonBookReader(builder.Configuration["LibraryJSOMPath"]));
+builder.Services.AddScoped<IReader<Book>>(reader => new Library.Services.JsonBookReader(builder.Configuration["LibraryJSONPath"]));
 
 var app = builder.Build();
 
